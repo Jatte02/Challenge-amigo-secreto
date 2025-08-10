daae545dbc74efba413a517275bc1f85dada8df6
Giancarlos Estevez <giancarlos.estevez@gmail.com>
2025-08-10 10:40:24 -0400

    Implementa Funcion para agregar amigos y limpiar campo

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let listaAmigos = []
+let listaAmigos = [];
+let entrada = document.getElementById('amigo');
+let texto = entrada.value;
 
-function sortearAmigo() {
+
+function agregarAmigo() {
+    let entrada = document.getElementById('amigo'); 
+    let texto = entrada.value.trim();
     
+    if (texto.length === 0) {
+        alert('Por favor, inserte un nombre.')
+        return;
+    } else {
+        listaAmigos.push(texto);
+        
+        entrada.value = "";   //Limpio el campo
+        entrada.focus({preventScroll: true})  //Devuelve el enfoque sin mover el scroll
+        console.log(listaAmigos);
+    } 
 }
